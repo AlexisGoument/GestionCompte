c801e5f104a857ad9be16f573b9bf9e80c3c5c30
AlexisGoument <alexis.nguyen.goument@gmail.com>
2025-06-16 15:57:56 +0200

    modif Program

--- GestionCompte/Program.cs
+++ GestionCompte/Program.cs
@@ -1,3 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using GestionCompte;
+
+const string csvPath = "../../../../ressources/account_20230228.csv";
+var reader = new HistoriqueCompteReader(new FileReader());
+var analyseur = new CompteAnalyseur(reader, csvPath);
+
+var hasOnlyOneArg = args.Length == 1;
+bool ArgIsNotDate(out DateOnly date) => !DateOnly.TryParse(args[0], out date);
+if (!hasOnlyOneArg || ArgIsNotDate(out DateOnly maxDate))
+{
+    WriteUsage();
+    return;
+}
+
+var rapport = analyseur.GetValeurADate(maxDate);
+Console.WriteLine(rapport.ToString());
+
+void WriteUsage()
+{
+    Console.WriteLine("Usage: GestionCompte.exe 01/01/2022");
+}
